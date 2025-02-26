2a3ceeb1377ddd2d632ad9a43922568dab8e2834
bruno.santos382 <bruno.santos382@fatec.sp.gov.br>
2025-02-26 19:17:46 -0300

    Implementar classe "Tesoura"

--- src/domain/Tesoura.java
+++ src/domain/Tesoura.java
@@ -1,8 +1,15 @@
 package domain;
 
-public class Tesoura extends  Algoritmo{
+public class Tesoura extends Algoritmo {
     @Override
     public void executar(TipoJogada tipo) {
-
+        switch (tipo) {
+            case PAPEL -> System.out.println("Ganhou - Tesoura corta Papel.");
+            case TESOURA -> System.out.println("Empatou - Tesoura empata com Tesoura.");
+            case LAGARTO -> System.out.println("Ganhou - Tesoura decapita Lagarto.");
+            case PEDRA -> System.out.println("Perdeu - Pedra é mais forte que Tesoura.");
+            case SPOCK -> System.out.println("Perdeu - Spock destrói Tesoura.");
+            default -> System.out.println("Opção inválida");
+        }
     }
 }
