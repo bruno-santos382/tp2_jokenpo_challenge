929c0febbf4e6c41ba5dd21877822e39e505b262
bruno.santos382 <bruno.santos382@fatec.sp.gov.br>
2025-02-26 19:15:55 -0300

    Implementar classe "Pedra"

--- src/domain/Pedra.java
+++ src/domain/Pedra.java
@@ -1,8 +1,15 @@
 package domain;
 
-public class Pedra extends Algoritmo{
+public class Pedra extends Algoritmo {
     @Override
     public void executar(TipoJogada tipo) {
-
+        switch (tipo) {
+            case PAPEL -> System.out.println("Perdeu - Papel cobre Pedra.");
+            case TESOURA -> System.out.println("Ganhou - Tesoura é quebrada por Pedra.");
+            case LAGARTO -> System.out.println("Ganhou - Lagarto é esmagado por Pedra.");
+            case PEDRA -> System.out.println("Empatou - Pedra empata com Pedra.");
+            case SPOCK -> System.out.println("Perdeu - Pedra é destruída por Spock.");
+            default -> System.out.println("Opção inválida");
+        }
     }
 }
